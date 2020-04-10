7be52fc180b4af17f2e53b3626030f4378137ad4
Ricardo Havill <rhavill@protonmail.com>
2020-04-10 18:12:38 -0400

    Made some improvements to the admin question listing page.

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,9 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
